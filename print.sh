b07eae6f12e994e4b69fe085eb4c1d42894c0755
Nanidini <86579620+Nanidini@users.noreply.github.com>
2021-07-28 11:33:20 +1000

    Add files via upload

---
+++ print.sh
@@ -0,0 +1,30 @@
+#echo "Provide GIT Repo"
+
+read $1
+
+echo "Your entered $1"
+#Read all files that are part of Git
+#echo "Provide localFolder to clone"
+
+#read $2
+
+echo "Your entered $2"
+git clone "$1" 
+
+fileNames=$(git -C $1 log origin/main --name-only --pretty=format: main)
+
+#convert variable to array
+
+IFS=$'\n' array=($fileNames)
+
+echo "------ Printing file names"
+
+for element in "${array[@]}"
+
+do  
+
+    echo "Trying to add White space in $element"
+
+    printf " " >> $element 
+
+done 
